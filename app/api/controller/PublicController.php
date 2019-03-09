14f888075561e8f3d98d5a83b33c1166a2c31cc4
liaoqiang <1083488361@qq.com>
2019-03-09 18:00:35 +0800

    移除短信测试

--- app/api/controller/PublicController.php
+++ app/api/controller/PublicController.php
@@ -25,7 +25,6 @@ class PublicController extends ApiBaseController
     }
     public function sendsms()
     {
-        return (uniqid());
         $validate = new Validate([
             'mobile' => 'require',
         ]);
